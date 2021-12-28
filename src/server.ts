58605fbdd889732cfec06101edf5f6e7a5133a82
myesn <myesn@foxmail.com>
2021-12-28 17:09:53 +0800

    完成 Node.js 集成 ESLint、TS ESLint、Prettier、TypeScript

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import Koa from 'koa';
+
+const server: Koa = new Koa();
+const port = 3000;
+
+server.use((ctx: Koa.DefaultContext) => {
+  ctx.body = 'hi koa';
+  const a = '123132';
+});
+
+server.listen(port, () => {
+  console.log(`Node.js v${process.versions.node}`);
+});
